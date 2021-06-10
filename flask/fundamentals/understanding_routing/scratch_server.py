831e03f57ba2400774a5cda7c65c253d34eed51d
MRea <rotavele666@pm.me>
2021-06-10 14:00:19 -0700

    Another Commit

--- flask/fundamentals/understanding_routing/scratch_server.py
+++ flask/fundamentals/understanding_routing/scratch_server.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Hello World!!'
+    return render_template('index.html', phrase= "hello",times =5)
+
+@app.route('/play')
+def play_boxes():
+    return render_template('index.html', phrase= "hello",times =5)
 
 @app.route('/dojo')
 def dojo():
@@ -11,11 +15,17 @@ def dojo():
 @app.route('/hello/<name>')
 def say_name(name):
     print(name)
-    return "Hi " + name + "!!"
+    return (f"Hi, {name} !!")
+
+@app.errorhandler(404)
+def not_found(bug):
+    return f"Sorry, pal. Page Not Found. Try Again"
     
 @app.route('/repeat/<int:number>/<greeting>')
 def repeat(number, greeting):
     print(greeting)
-    return greeting * number
+    return (f" {greeting} ") * number
+
 if __name__=="__main__":
     app.run(debug=True)
+    
