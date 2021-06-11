df3a2899523ec544c5820661cd6ae73a0b989713
MRea <rotavele666@pm.me>
2021-06-10 18:58:31 -0700

    Another Commit

--- flask/fundamentals/Playground/playground.py
+++ flask/fundamentals/Playground/playground.py
@@ -1,19 +1,17 @@
 from flask import Flask, render_template
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/play')
-def play_boxes():
+@app.route('/play/')
+def blue_boxes():
+    return render_template('index.html')
 
+@app.route('/play/<int:num>')
+def num_boxes(num):
+    return render_template('index_2.html', times = num)
 
-
-
-
-
-
-
-
-
-
-
-if __name__="__main_":
-app.run(debug=True)
+@app.route('/play/<int:num>/<color>')
+def color_boxes(num, color):
+    return render_template('index_2.html', times = num, color = )
+    
+if __name__=="__main__":
+    app.run(debug=True)
